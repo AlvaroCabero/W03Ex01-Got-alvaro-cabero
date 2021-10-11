975dc1b3bd4fa85fd2cff169a191a6da6d2d4dab
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-11 13:13:26 +0200

    :arrow_up: Added Assesor Class

---
+++ Asesor.js
@@ -0,0 +1,11 @@
+class Luchador extends Personaje{
+  asesorado;
+  constructor(asesorado ){
+  super(name, family, age, alive){
+    this.asesorado = weapon;
+    
+} 
+  }
+comunica_luchador(){
+  console.log ("No sé por qué, pero creo que voy a morir pronto");
+}
