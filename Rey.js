3ec380381a5323946df94a93f3dd19d123492d0a
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-11 13:10:42 +0200

    :arrow_up: Added Luchador Class

--- Rey.js
+++ Rey.js
@@ -1,4 +1,10 @@
 class Rey extends Personaje{
-  constructor(name, )
+  reinado;
+  constructor(reinado ){
   super(name, family, age, alive){
+    this.reinado
+}
+  }
+comunica_rey(){
+  console.log ("Vais a morir todos");
 }
